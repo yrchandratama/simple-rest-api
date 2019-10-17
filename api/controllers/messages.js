981bc259a372e7432e39eb6ac0651694ff7c29f8
Yudi <yudi@yudi.local>
2019-10-17 14:17:38 +0700

    better error handler

--- api/controllers/messages.js
+++ api/controllers/messages.js
@@ -1,25 +1,24 @@
-const { ValidationError } = require('../error');
+const { validationResult } = require('express-validator');
 
 let messages = [];
 
 exports.sendMessage = async (req, res) => {
     const message = req.body.message;
 
-    if (!message || message.length === 0) {
-        const err = new ValidationError('MESSAGE_SHOULD_NOT_BE_EMPTY');
-        return res.status(400).json({ error: { name: err.name, message: err.message } });
+    let error = validationResult(req);
+    if (!error.isEmpty()) {
+        error = error.array()[0];
+        
+        return res.status(422).json({
+            error: { param: error.param, message: error.msg }
+        });
     }
 
     await messages.push(message);
-    
+
     return res.status(201).json({ status: 'ok', message: message });
 }
 
 exports.getMessages = (req, res) => {
-    if (messages.length === 0) {
-        const err = new ValidationError('MESSAGE_IS_EMPTY');
-        return res.status(400).json({ error: { name: err.name, message: err.message } });
-    }
-
     res.status(200).json({ status: 'ok', count: messages.length, messages: messages });
 }
--- api/routes/messages.js
+++ api/routes/messages.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+const { check } = require('express-validator');
+
 const MessagesController = require('../controllers/messages');
 
-router.post('/', MessagesController.sendMessage);
+router.post(
+    '/', 
+    [
+        check('message').exists().withMessage("PARAM_IS_INVALID"), 
+        check('message').isLength({ min: 1 }).withMessage('MESSAGE_SHOULD_NOT_BE_EMPTY'),
+    ],
+    MessagesController.sendMessage
+);
 
-router.get('/', MessagesController.getMessages);
+router.get(
+    '/',
+    MessagesController.getMessages
+);
 
 module.exports = router;
